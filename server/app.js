728e14219594c18c8fee4957cf2d275915eed47c
JahunSeo <wkgjs0809@gmail.com>
2021-11-05 14:57:42 +0900

    [johnny] server/ add middleware morgan

--- server/app.js
+++ server/app.js
@@ -1,14 +1,19 @@
 require("dotenv").config();
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
-const port = process.env.PORT || 5000;
+// using HTTP request logger middleware
+app.use(morgan("dev"));
 
+// api for test
 app.get("/api/test", (req, res) => {
   res.status(200).json({
     data: "Hello, Alien!",
   });
 });
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`** Raising Alien Creatures Web Server **`);
