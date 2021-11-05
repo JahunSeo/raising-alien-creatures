8d938f6b2cd8d3a6898300d26c103e2dbbbfd84e
JahunSeo <wkgjs0809@gmail.com>
2021-11-05 14:17:29 +0900

    [johnny] server/ set PORT as environmental variable using dotenv

--- server/app.js
+++ server/app.js
@@ -1,7 +1,8 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("Hello, Alien!");
