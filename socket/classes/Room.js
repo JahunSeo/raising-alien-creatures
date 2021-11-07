8ade16fe8b7210b0b30a6c6eced6c9100d4becec
JahunSeo <wkgjs0809@gmail.com>
2021-11-08 02:09:37 +0900

    [johnny] socket/ rebuild socket with classes

--- socket/classes/Room.js
+++ socket/classes/Room.js
@@ -1,46 +1,52 @@
-export default class Room {
+class Room {
   constructor(roomId) {
     this.roomId = roomId;
-    this.fieldState = null;
+    this.clientCnt = 0; // TODO: 접속해 있는 사람 수 개념으로 분리
+    this.participant = {};
+    this.initFieldState();
   }
 
-  initFieldState(roomId) {
-    console.log("initFieldState", roomId);
+  initFieldState() {
     // init field
     // TODO: 서버에서 해당 어항에 포함된 몬스터들을 가져오기
     const state = {
       monsters: [],
     };
     this.fieldState = state;
-    this.clientCnt = 0;
   }
 
-  addClient(clientId) {
-    const monster = this.initMonster(clientId);
+  getFieldState() {
+    return this.fieldState;
+  }
+
+  addParticipant(user) {
+    // 몬스터 추가
+    const monster = this.initMonster(user.userId);
     this.fieldState.monsters.push(monster);
-    // TODO from other place
-    // mapClientToRoom[socketId] = roomId;
+    // 참가자 추가
+    this.participant[user.userId] = user;
     this.clientCnt += 1;
-    return this.clientCnt;
+    return true;
   }
 
-  removeClient(clientId) {
+  removeParticipant(user) {
+    // 몬스터 제거
     this.fieldState.monsters = this.fieldState.monsters.filter(
-      (mon) => mon.clientId !== clientId
+      (mon) => mon.userId !== user.userId
     );
+    // 참가자 제거
+    delete this.participant[user.userId];
     this.clientCnt -= 1;
     return this.clientCnt;
-    // TODO from other place
-    // delete mapClientToRoom[socketId];
   }
 
-  initMonster(clientId) {
+  initMonster(userId) {
     // generate random monster
     let randRange = 300;
     let x = (Math.random() - 0.5) * randRange;
     let y = (Math.random() - 0.5) * randRange;
     const monster = {
-      clientId, // TODO: user id or email
+      userId, // TODO: user id or email
       position: { x, y },
       destination: { x, y },
       size: 50 + Math.random() * 100,
@@ -49,9 +55,9 @@ export default class Room {
     return monster;
   }
 
-  updateMonster(clientId, features) {
+  updateMonster(userId, features) {
     this.fieldState.monsters = this.fieldState.monsters.map((mon) => {
-      if (mon.clientId !== clientId) return mon;
+      if (mon.userId !== userId) return mon;
       return {
         ...mon,
         ...features,
@@ -68,3 +74,5 @@ export default class Room {
     return color;
   }
 }
+
+module.exports = Room;
