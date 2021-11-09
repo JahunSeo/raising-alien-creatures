3fef6fe491a6548d54b197019aef4f63b550ef82
JahunSeo <wkgjs0809@gmail.com>
2021-11-09 15:46:43 +0900

    [johnny] relocate .env file

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const dotenv = require("dotenv");
-dotenv.config({ path: path.join(__dirname, "/.env") });
+dotenv.config({ path: path.join(__dirname, "../.env") });
 
 const express = require("express");
 const cookieParser = require("cookie-parser");
