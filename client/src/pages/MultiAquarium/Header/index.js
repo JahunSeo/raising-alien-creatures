2d2811df50295c27503389a299abe67f4f4b73e5
JahunSeo <wkgjs0809@gmail.com>
2021-11-10 16:15:00 +0900

    [johnny] restyling header:

--- client/src/pages/MultiAquarium/Header/index.js
+++ client/src/pages/MultiAquarium/Header/index.js
@@ -1,20 +1,28 @@
 import React from "react";
 import styles from "./index.module.css";
 
+import classNames from "classnames/bind";
+const cx = classNames.bind(styles);
+
 export default function Header(props) {
   const { rooms, roomId, setRoomId } = props;
 
   return (
     <div className={styles.body}>
-      <h1>Test Aquarium</h1>
-      <p>Current Room: {roomId}</p>
-      <div>
+      <div className={cx("item", "itemTitle")}>
+        <h1 className={styles.title}>{`Aquarium: ROOM ${roomId}`}</h1>
+      </div>
+      <div className={cx("item", "itemRoom")}>
         {rooms.map((roomId) => (
           <button
             key={roomId}
             onClick={() => setRoomId(roomId)}
           >{`Room ${roomId}`}</button>
         ))}
+      </div>
+      <div className={cx("item", "itemUser")}>
+        <button>로그인</button>
+        <button>회원가입</button>
       </div>
     </div>
   );
