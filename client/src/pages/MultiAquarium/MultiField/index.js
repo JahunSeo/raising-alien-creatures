6bf87a6c27286efb3518107591f49b3c01d253d0
JahunSeo <wkgjs0809@gmail.com>
2021-11-11 01:30:07 +0900

    [johnny] translate screen by camera center /ing

--- client/src/pages/MultiAquarium/MultiField/index.js
+++ client/src/pages/MultiAquarium/MultiField/index.js
@@ -24,17 +24,20 @@ export default class Field extends Component {
     ctx.save();
     ctx.clearRect(0, 0, cvsWidth, cvsHeight);
 
-    // translate location
-    ctx.translate(cvsWidth / 2, cvsHeight / 2);
-
-    // draw background
-    ctx.fillStyle = `rgba(255, 255, 255, 1)`;
-    ctx.fillRect(0, 0, cvsWidth, cvsHeight);
-
-    // draw monster
     if (this.props.room && this.props.room.fieldState) {
       const room = this.props.room;
       const { monsters } = room.fieldState;
+
+      // translate location
+      const { center } = room.camera;
+      ctx.translate(cvsWidth / 2 - center.x, cvsHeight / 2 - center.y);
+      // room.camera.center.x++;
+
+      // draw background
+      ctx.fillStyle = `rgba(255, 255, 255, 1)`;
+      ctx.fillRect(0, 0, cvsWidth, cvsHeight);
+
+      // draw monster
       // TODO: monster들의 순서 (누가 위에 놓일 것인지 여부) 처리 필요
       for (const userId in monsters) {
         let { location, size, color } = monsters[userId];
@@ -48,12 +51,12 @@ export default class Field extends Component {
       }
 
       if (mouseObj.clicked) {
-        let destination = {
-          x: mouseObj.deltaXfromCenter,
-          y: mouseObj.deltaYfromCenter,
-        };
-        destination = room.camera.getLocalFromMouse(destination);
-        socket.changeDestination(room.roomId, destination);
+        // let destination = {
+        //   x: mouseObj.deltaXfromCenter,
+        //   y: mouseObj.deltaYfromCenter,
+        // };
+        // destination = room.camera.getLocalFromMouse(destination);
+        // socket.changeDestination(room.roomId, destination);
       }
     }
 
