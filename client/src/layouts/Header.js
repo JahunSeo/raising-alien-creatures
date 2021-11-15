b30587933f1fa6c944ebd4ee5b4e1e5c213c6c95
shinhojung814 <shinhojung814@gmail.com>
2021-11-15 22:36:05 +0900

    [Signal] sign-in success (feat.jyk)

--- client/src/layouts/Header.js
+++ client/src/layouts/Header.js
@@ -1,53 +1,87 @@
-import React, { useState } from 'react';
-import { Button, Container, Nav, Navbar } from 'react-bootstrap';
-import SignUpModal from '../modals/SignUpModal';
-import SignInModal from '../modals/SignInModal';
+import React, { useState } from "react";
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
+import SignUpModal from "../modals/SignUpModal";
+import SignInModal from "../modals/SignInModal";
 
 const Header = () => {
-    const [signUpModalOn, setSignUpModalOn] = useState(false);
-    const [signInModalOn, setSignInModalOn] = useState(false);
+  const [logInStatus, setLoginStatus] = useState(false);
+  const [signUpModalOn, setSignUpModalOn] = useState(false);
+  const [signInModalOn, setSignInModalOn] = useState(false);
 
-    return (
-        <>
-            <SignUpModal
-                show={signUpModalOn}
-                onHide={() => setSignUpModalOn(false)}
-            />
-            <SignInModal
-                show={signInModalOn}
-                onHide={() => setSignInModalOn(false)}
-            />
+  return (
+    <div>
+      <SignUpModal
+        show={signUpModalOn}
+        onHide={() => setSignUpModalOn(false)}
+        setSignUpModalOn={setSignUpModalOn}
+      />
+      <SignInModal
+        show={signInModalOn}
+        onHide={() => setSignInModalOn(false)}
+        setSignInModalOn={setSignInModalOn}
+      />
 
-            <header>
-                <Navbar bg="light" expand="lg">
-                    <Container>
-                        <Navbar.Brand>목표달성! 외계생물 기르기</Navbar.Brand>
-                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                        <Navbar.Collapse id="basic-navbar-nav">
-                            <Nav className="ml-auto">
-                                <Nav.Link>
-                                    <Button
-                                        variant="secondary"
-                                        onClick={() => setSignUpModalOn(true)}
-                                    >
-                                        회원가입
-                                    </Button>
-                                </Nav.Link>
-                                <Nav.Link>
-                                    <Button
-                                        variant="primary"
-                                        onClick={() => setSignInModalOn(true)}
-                                    >
-                                        로그인
-                                    </Button>
-                                </Nav.Link>
-                            </Nav>
-                        </Navbar.Collapse>
-                    </Container>
-                </Navbar>
-            </header>
-        </>
-    )
-}
+      {loginStatus ? (
+        <header>
+          <Navbar bg="light" expand="lg">
+            <Container>
+              <Navbar.Brand>목표달성! 외계생물 기르기</Navbar.Brand>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="ml-auto">
+                  <Nav.Link>
+                    <Button
+                      variant="secondary"
+                      onClick={() => setSignUpModalOn(true)}
+                    >
+                      회원정보
+                    </Button>
+                  </Nav.Link>
+                  <Nav.Link>
+                    <Button
+                      variant="primary"
+                      onClick={() => setSignInModalOn(true)}
+                    >
+                      로그아웃
+                    </Button>
+                  </Nav.Link>
+                </Nav>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
+        </header>
+      ) : (
+        <header>
+          <Navbar bg="light" expand="lg">
+            <Container>
+              <Navbar.Brand>목표달성! 외계생물 기르기</Navbar.Brand>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="ml-auto">
+                  <Nav.Link>
+                    <Button
+                      variant="secondary"
+                      onClick={() => setSignUpModalOn(true)}
+                    >
+                      회원가입
+                    </Button>
+                  </Nav.Link>
+                  <Nav.Link>
+                    <Button
+                      variant="primary"
+                      onClick={() => setSignInModalOn(true)}
+                    >
+                      로그인
+                    </Button>
+                  </Nav.Link>
+                </Nav>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
+        </header>
+      )}
+    </div>
+  );
+};
 
 export default Header;
