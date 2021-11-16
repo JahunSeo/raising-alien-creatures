6c22ea4ea85e3c638916b442559535ccb55aa0a4
JahunSeo <wkgjs0809@gmail.com>
2021-11-17 02:02:04 +0900

    [johnny] plaza logic added

--- client/src/pages/MultiAquarium/Room/PlazaRoom.js
+++ client/src/pages/MultiAquarium/Room/PlazaRoom.js
@@ -1,5 +1,42 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Room from "../../../shared/room/RoomClient";
+
+import api from "../../../apis";
 
 export default function PlazaRoom(props) {
+  // 챌린지 정보 가져오기
+  const roomId = "plaza";
+  useEffect(() => {
+    const { rooms, setCurrRoomId, setAliens } = props;
+    try {
+      const fetchData = async () => {
+        const res = await api.get("/main");
+        console.log("fetch main data", res.data);
+        if (res.data.result === "success") {
+          // 서버에서 데이터를 받아온 상황을 전제로 구성
+
+          // rooms 상태 정보
+          if (!rooms.current) rooms.current = {};
+          rooms.current[roomId] = new Room(roomId);
+          rooms.current[roomId].initMonsters(res.data.data);
+          rooms.current[roomId].start();
+
+          // roomIds: react에서 state로 관리할 정보
+          setCurrRoomId(roomId);
+          //
+          setAliens(res.data.data);
+          console.log("rooms", rooms.current);
+        } else {
+        }
+      };
+      fetchData();
+    } catch (err) {
+      console.error("fetchData fail", err);
+    }
+    return () => {
+      rooms.current[roomId].close();
+    };
+  }, []);
+
   return <div></div>;
 }
--- client/src/pages/MultiAquarium/index.js
+++ client/src/pages/MultiAquarium/index.js
@@ -16,60 +16,68 @@ import styles from "./index.module.css";
 export default function MultiAquarium() {
   const [currRoomId, setCurrRoomId] = useState(null);
   const rooms = useRef();
-
   const [aliens, setAliens] = useState([]);
 
-  // 챌린지 정보 가져오기
-  useEffect(() => {
-    try {
-      const fetchData = async () => {
-        const res = await api.get("/main");
-        console.log("fetch main data", res.data);
-        if (res.data.result === "success") {
-          // 서버에서 데이터를 받아온 상황을 전제로 구성
-          let roomId = "plaza";
+  // // 챌린지 정보 가져오기
+  // useEffect(() => {
+  //   try {
+  //     const fetchData = async () => {
+  //       const res = await api.get("/main");
+  //       console.log("fetch main data", res.data);
+  //       if (res.data.result === "success") {
+  //         // 서버에서 데이터를 받아온 상황을 전제로 구성
+  //         let roomId = "plaza";
 
-          // rooms 상태 정보
-          rooms.current = {};
-          rooms.current[roomId] = new Room(roomId);
-          rooms.current[roomId].initMonsters(res.data.data);
+  //         // rooms 상태 정보
+  //         rooms.current = {};
+  //         rooms.current[roomId] = new Room(roomId);
+  //         rooms.current[roomId].initMonsters(res.data.data);
 
-          // roomIds: react에서 state로 관리할 정보
-          setCurrRoomId(roomId);
-          //
-          setAliens(res.data.data);
-          console.log("rooms", rooms.current);
-        } else {
-        }
-      };
-      fetchData();
-    } catch (err) {
-      console.error("fetchData fail", err);
-    }
-  }, []);
+  //         // roomIds: react에서 state로 관리할 정보
+  //         setCurrRoomId(roomId);
+  //         //
+  //         setAliens(res.data.data);
+  //         console.log("rooms", rooms.current);
+  //       } else {
+  //       }
+  //     };
+  //     fetchData();
+  //   } catch (err) {
+  //     console.error("fetchData fail", err);
+  //   }
+  // }, []);
 
-  useEffect(() => {
-    // rooms가 생성되었는지 확인
-    if (!rooms.current || !currRoomId) return;
+  // useEffect(() => {
+  //   // rooms가 생성되었는지 확인
+  //   if (!rooms.current || !currRoomId) return;
 
-    // 해당 room에 조인
-    console.log("set currRoomId", currRoomId);
-    // socket.initAndJoin(currRoomId);
-    // socket.subscribe(rooms.current[currRoomId].syncFieldState);
-    // room의 update logic start
-    rooms.current[currRoomId].start();
+  //   // 해당 room에 조인
+  //   console.log("set currRoomId", currRoomId);
+  //   // socket.initAndJoin(currRoomId);
+  //   // socket.subscribe(rooms.current[currRoomId].syncFieldState);
+  //   // room의 update logic start
+  //   rooms.current[currRoomId].start();
 
-    return () => {
-      // socket.disconnect();
-      rooms.current[currRoomId].close();
-    };
-  }, [currRoomId]);
+  //   return () => {
+  //     // socket.disconnect();
+  //     rooms.current[currRoomId].close();
+  //   };
+  // }, [currRoomId]);
 
   console.log("[MultiAquarium] currRoomId", currRoomId, rooms);
   return (
     <div className={styles.body}>
       <Routes>
-        <Route path="/" element={<PlazaRoom />}></Route>
+        <Route
+          path="/"
+          element={
+            <PlazaRoom
+              rooms={rooms}
+              setCurrRoomId={setCurrRoomId}
+              setAliens={setAliens}
+            />
+          }
+        ></Route>
         <Route path="/user/:userId" element={<UserRoom />}></Route>
       </Routes>
 
