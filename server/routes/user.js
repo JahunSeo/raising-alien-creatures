6fbf170739fb49ac749571d999783e097fcaf897
JahunSeo <wkgjs0809@gmail.com>
2021-11-17 21:35:19 +0900

    [johnny] server/ login api, return with user id

--- server/routes/user.js
+++ server/routes/user.js
@@ -64,6 +64,7 @@ module.exports = function (passport, connection) {
             result: "success",
             email: req.user.email,
             nickname: req.user.nickname,
+            id: req.user.id,
           };
           res.json(result);
         });
@@ -210,21 +211,24 @@ module.exports = function (passport, connection) {
   router.get("/:userId", (req, res) => {
     const user_id = req.params.userId;
     try {
-    connection.query('(SELECT * FROM Alien JOIN Challenge ON Challenge.id = Alien.Challenge_id WHERE Alien.user_info_id = ?) UNION (SELECT * FROM Alien_graduated JOIN Challenge ON Challenge.id = Alien_graduated.Challenge_id WHERE Alien_graduated.user_info_id = ?)', [user_id, user_id], function(err, result) {
-      res.status(200).json({
-        result: "success",
-        data: result,
-        });
-    })
-  } catch (err) {
-    console.error(err);
-    res.status(501).json({
-      result: "fail",
-      msg: "cant select infomations"
-    });  
-  }
+      connection.query(
+        "(SELECT * FROM Alien JOIN Challenge ON Challenge.id = Alien.Challenge_id WHERE Alien.user_info_id = ?) UNION (SELECT * FROM Alien_graduated JOIN Challenge ON Challenge.id = Alien_graduated.Challenge_id WHERE Alien_graduated.user_info_id = ?)",
+        [user_id, user_id],
+        function (err, result) {
+          res.status(200).json({
+            result: "success",
+            data: result,
+          });
+        }
+      );
+    } catch (err) {
+      console.error(err);
+      res.status(501).json({
+        result: "fail",
+        msg: "cant select infomations",
+      });
+    }
   });
- 
 
   router.use(function (req, res, next) {
     res.status(404).send("Sorry cant find that!");
