b0090564c28d36b6e3140a605c7ec495f0ed8f8e
Ratel <sambujakkk@naver.com>
2021-11-20 21:47:59 +0900

    [JJong] pooling connection 도입

--- server/routes/user.js
+++ server/routes/user.js
@@ -10,34 +10,37 @@ var isOwner = (req, res) => {
   }
 };
 
-module.exports = function (passport, connection) {
+module.exports = function (passport, pool) {
   router.post("/register", (req, res, next) => {
     const data = req.body;
     const email = data.userEmail;
     const password = data.userPassword;
     const encryptedPassword = bcrypt.hashSync(password, 10);
     const nickname = data.userNickname;
-    connection.query(
-      "INSERT INTO user_info (email, password, nickname) values (?, ?, ?)",
-      [email, encryptedPassword, nickname],
-      function (err, results) {
-        if (err) {
-          console.error('at the user api', err);
-          err_handling = err.sqlMessage.split(" ");
-          const duplicated_email = err_handling[2];
-          res.json({
-            result: "fail",
-            msg: `${duplicated_email}은(는) 이미 존재하는 이메일 주소입니다.`,
-          });
-          return;
+    pool.getConnection(function(err, connection){
+      connection.query(
+        "INSERT INTO user_info (email, password, nickname) values (?, ?, ?)",
+        [email, encryptedPassword, nickname],
+        function (err, results) {
+          if (err) {
+            console.error('at the user api', err);
+            err_handling = err.sqlMessage.split(" ");
+            const duplicated_email = err_handling[2];
+            res.json({
+              result: "fail",
+              msg: `${duplicated_email}은(는) 이미 존재하는 이메일 주소입니다.`,
+            });
+            return;
+          }
+          console.log(
+            "server has registered new user`s information successfully.",
+            results
+          );
+          res.json({ result: "success", msg: "user registered" });
+          connection.release();
         }
-        console.log(
-          "server has registered new user`s information successfully.",
-          results
-        );
-        res.json({ result: "success", msg: "user registered" });
-      }
-    );
+      );
+    });
   });
 
   router.get("/login/confirm", (req, res) => {
@@ -80,29 +83,31 @@ module.exports = function (passport, connection) {
     var sql3 = `select * from Alien_graduated where Alien_graduated.user_info_id=${user_info_id};`;
     var sql4 = `select challengeName, challengeContent, createDate, createUserNickName, maxUserNumber, participantNumber, cntOfWeek, Challenge_id from Challenge  inner join user_info_has_Challenge on Challenge.id = user_info_has_Challenge.Challenge_id  inner join user_info on user_info_has_Challenge.user_info_id = user_info.id  where user_info.id = ${user_info_id};`;
     var sql5 = `select * from Authentification inner join user_info_has_Challenge on user_info_has_Challenge.Challenge_id = Authentification.Challenge_id where user_info_has_Challenge.user_info_id = ${user_info_id} and isAuth = 0;`;
-    connection.query(
-      sql1 + sql2 + sql3 + sql4 + sql5,
-      function (error, results, fields) {
-        if (error) {
-          console.error(error);
-          res.json({
-            result: "fail",
-            msg: "Fail to load Information from Database.",
-          });
-
-          return;
+    pool.getConnection(function(err, connection){
+      connection.query(
+        sql1 + sql2 + sql3 + sql4 + sql5,
+        function (error, results, fields) {
+          if (error) {
+            console.error(error);
+            res.json({
+              result: "fail",
+              msg: "Fail to load Information from Database.",
+            });
+            return;
+          }
+          var result = {
+            result: "success",
+            Alien: results[0],
+            Alien_dead: results[1],
+            Alien_graduated: results[2],
+            Challenge: results[3],
+            notice: results[4],
+          };
+          res.json(result);
         }
-        var result = {
-          result: "success",
-          Alien: results[0],
-          Alien_dead: results[1],
-          Alien_graduated: results[2],
-          Challenge: results[3],
-          notice: results[4],
-        };
-        res.json(result);
-      }
-    );
+      );
+      connection.release();
+    });
   });
 
   //챌린지 어장가기
@@ -118,25 +123,27 @@ module.exports = function (passport, connection) {
     var sql1 = `select * from Alien where Alien.Challenge_id = ${challenge_id};`;
     var sql2 = `select * from Alien_dead where Alien_dead.Challenge_id = ${challenge_id};`;
     var sql3 = `select * from Alien_graduated where Alien_graduated.Challenge_id=${challenge_id};`;
-    connection.query(sql1 + sql2 + sql3, function (error, results, fields) {
-      if (error) {
-        console.error(error);
-        res.json({
-          result: "fail",
-          msg: "Fail to load Information from Database.",
-        });
-        return;
-      }
-      var result = {
-        result: "success",
-        user: user_id,
-        nickname: user_nickname,
-        Alien: results[0],
-        Alien_dead: results[1],
-        Alien_graduated: results[2],
-      };
-
-      res.json(result);
+    pool.getConnection(function(err, connection){
+      connection.query(sql1 + sql2 + sql3, function (error, results, fields) {
+        if (error) {
+          console.error(error);
+          res.json({
+            result: "fail",
+            msg: "Fail to load Information from Database.",
+          });
+          return;
+        }
+        var result = {
+          result: "success",
+          user: user_id,
+          nickname: user_nickname,
+          Alien: results[0],
+          Alien_dead: results[1],
+          Alien_graduated: results[2],
+        };
+        res.json(result);
+        connection.release();
+      });
     });
   });
 
@@ -158,24 +165,27 @@ module.exports = function (passport, connection) {
 
   router.get("/:userId", (req, res) => {
     const user_id = req.params.userId;
-    connection.query(
-      "(SELECT Alien.id, status, challengeName, challengeContent, Alien.Challenge_id, Challenge.createDate, createUserNickName, maxUserNumber, participantNumber, Alien.createDate, alienName, accuredAuthCnt, color, graduate_toggle, user_info_id, week_auth_cnt, total_auth_cnt, auth_day, alive_date FROM Challenge JOIN Alien ON Challenge.id = Alien.Challenge_id WHERE Alien.user_info_id = ?) UNION (SELECT Alien_graduated.id, status, challengeName, challengeContent, Alien_graduated.Challenge_id, Challenge.createDate, createUserNickName, maxUserNumber, participantNumber, Alien_graduated.createDate, alienName, accuredAuthCnt, color, graduate_toggle, user_info_id, week_auth_cnt, total_auth_cnt, auth_day, graduated_date FROM Challenge JOIN Alien_graduated ON Challenge.id = Alien_graduated.Challenge_id WHERE Alien_graduated.user_info_id = ?)",
-      [user_id, user_id],
-      function (err, result) {
-        if (err) {
-          console.error(err);
+    pool.getConnection(function(err, connection){
+      connection.query(
+        "(SELECT Alien.id, status, challengeName, challengeContent, Alien.Challenge_id, Challenge.createDate, createUserNickName, maxUserNumber, participantNumber, Alien.createDate, alienName, accuredAuthCnt, color, graduate_toggle, user_info_id, week_auth_cnt, total_auth_cnt, auth_day, alive_date FROM Challenge JOIN Alien ON Challenge.id = Alien.Challenge_id WHERE Alien.user_info_id = ?) UNION (SELECT Alien_graduated.id, status, challengeName, challengeContent, Alien_graduated.Challenge_id, Challenge.createDate, createUserNickName, maxUserNumber, participantNumber, Alien_graduated.createDate, alienName, accuredAuthCnt, color, graduate_toggle, user_info_id, week_auth_cnt, total_auth_cnt, auth_day, graduated_date FROM Challenge JOIN Alien_graduated ON Challenge.id = Alien_graduated.Challenge_id WHERE Alien_graduated.user_info_id = ?)",
+        [user_id, user_id],
+        function (err, result) {
+          if (err) {
+            console.error(err);
+            res.status(200).json({
+              result: "fail",
+              msg: "cant select infomations",
+            });
+            return;
+          }
           res.status(200).json({
-            result: "fail",
-            msg: "cant select infomations",
+            result: "success",
+            data: result,
           });
-          return;
         }
-        res.status(200).json({
-          result: "success",
-          data: result,
-        });
-      }
-    );
+      );
+      connection.release();
+    });
   });
 
   router.use(function (req, res, next) {
