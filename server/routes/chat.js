65c63d8e1345a1dc8695cf9904c0d14a66c50c50
JahunSeo <wkgjs0809@gmail.com>
2021-11-21 19:37:55 +0900

    [johnny] add api/chat/create

--- server/routes/chat.js
+++ server/routes/chat.js
@@ -5,7 +5,7 @@ module.exports = function (pool) {
   // add chat message
   router.post("/create", function (req, res) {
     console.log(req.body);
-    // login 상태 확인
+    // 1단계: login 상태 확인
     if (!req.user) {
       res.status(401).json({
         result: "fail",
@@ -13,12 +13,51 @@ module.exports = function (pool) {
       });
       return;
     }
-    // TODO: challenge 참가 중인지 여부 체크
-
-    // TODO: add msg
-    res.status(201).json({
-      result: "success",
-      msg: "do insert",
+    // 2단계: 저장할 값 validation check
+    let user_info_id = parseInt(req.user.id);
+    let challenge_id = parseInt(req.body.challenge_id);
+    let msg_text = req.body.msg_text;
+    if (!user_info_id || !challenge_id) {
+      res.status(400).json({
+        result: "fail",
+        msg: "Bad Request: challenge_id should be provided.",
+      });
+      return;
+    }
+    pool.getConnection(function (err, connection) {
+      if (err) throw err; // not connected!
+      // 3단계: user가 challenge에 참가중인지 확인
+      connection.query(
+        "SELECT * from user_info_has_challenge where user_info_id=? and challenge_id=?",
+        [req.user.id, req.body.challenge_id],
+        function (err, results) {
+          if (err) throw err; // server error!
+          if (results.length <= 0) {
+            res.status(403).json({
+              result: "fail",
+              msg: "Forbidden",
+            });
+            return;
+          }
+          // 4단계: message 추가
+          connection.query(
+            "INSERT INTO chat_message SET ?",
+            {
+              user_info_id,
+              challenge_id,
+              msg_text,
+            },
+            function (err, results) {
+              if (err) throw err; // server error!
+              res.status(201).json({
+                result: "success",
+                msg: "do insert",
+              });
+              return;
+            }
+          );
+        }
+      );
     });
   });
 
