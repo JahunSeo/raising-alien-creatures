41ab779680db86b59b42eb6e951cdd3e30b4d48c
Ratel <sambujakkk@naver.com>
2021-11-23 12:35:06 +0900

    [JJong] 챌린지 생성폼 api연결확인

--- client/src/pages/NewChallenge/index.js
+++ client/src/pages/NewChallenge/index.js
@@ -8,9 +8,9 @@ export default function NewAlien(props) {
   const SELECT_DEFAULT = 0;
   const [challengeTitle, setChallengeTitle] = useState("");
   const [challengeDescription, setChallengeDescription] = useState("");
-  const [challengeCapacity, setChallengeCapacity] = useState(SELECT_DEFAULT);
+  const [challengeCapacity, setValue] = useState(0);
   const [challengeFrequency, setChallengeFrequency] = useState(SELECT_DEFAULT);
-  const [challengeTag, setChallengeTag] = useState("");
+  const [challengeTag, setChallengeTag] = useState(SELECT_DEFAULT);
   const [challengeMessage, setChallengeMessage] = useState(null);
   
 
@@ -22,8 +22,8 @@ export default function NewAlien(props) {
     setChallengeDescription(e.target.value);
   }
 
-  function handleChallengeCapacity(e) {
-    setChallengeCapacity(e.target.value);
+  function handleChallengeCapacity(challengeCapacity) {
+    setValue(challengeCapacity)
   }
 
   function handleChallengeFrequency(e) {
@@ -42,7 +42,8 @@ export default function NewAlien(props) {
         challengeTitle,
         challengeDescription,
         challengeCapacity,
-        challengeFrequency
+        challengeFrequency,
+        challengeTag,
       )
     )
       return;
@@ -61,9 +62,9 @@ export default function NewAlien(props) {
     if (
       challengeTitle === "" ||
       challengeDescription === "" ||
-      challengeCapacity === SELECT_DEFAULT ||
+      challengeCapacity === 0 ||
       challengeFrequency === SELECT_DEFAULT ||
-      challengeTag === ""
+      challengeTag === SELECT_DEFAULT
     ) {
       setChallengeMessage("입력하지 않은 챌린지 정보가 있습니다.");
       return false;
@@ -82,14 +83,13 @@ export default function NewAlien(props) {
       tag: challengeTag,
     };
     const res = await api.post("/challenge/create", challengeData);
-    console.log("res", res);
     if (res.data.result === "success") {
       console.log("challengeData", challengeData);
       setChallengeTitle("");
       setChallengeDescription("");
-      setChallengeCapacity(SELECT_DEFAULT);
       setChallengeFrequency(SELECT_DEFAULT);
-      setChallengeTag("");
+      setChallengeTag(SELECT_DEFAULT);
+      setValue(0);
       alert("챌린지 생성에 성공하였습니다.");
       return;
     } else {
@@ -104,13 +104,13 @@ export default function NewAlien(props) {
     <div class="w-1/2 h-full bg-white rounded shadow-2xl p-8 m-4">
         <h1 class="block w-full text-center text-gray-800 text-2xl font-bold mb-6">새로운 챌린지 생성</h1>
             <div class="flex flex-col mb-4 p-4">
-                <label class="mb-2 font-bold text-lg text-gray-900" for="first_name">챌린지 이름</label>
-                <input class="border py-2 px-3 text-grey-800" type="text" name="first_name" id="first_name" />
+                <label class="mb-2 font-bold text-lg text-gray-900" for="challenge_name">챌린지 이름</label>
+                <input class="border py-2 px-3 text-grey-800" type="text" name="challenge_name" id="challenge_name" value={challengeTitle} onChange={handleChallengeTitle}/>
             </div>
 
             <div class="main flex border rounded-full overflow-hidden m-4 select-none ">
               <div class="title py-5 my-auto px-3 bg-indigo-500 text-white text-sm font-semibold mr-3">Category</div>
-              <select class="border py-2 px-4 text-grey-800">
+              <select class="border py-2 px-4 text-grey-800" value={challengeTag} onChange={handleChallengeTag}>
                 <option value={SELECT_DEFAULT}>선택</option>
                 <option value="운동">운동</option>
                 <option value="실생활">실생활</option>
@@ -122,7 +122,7 @@ export default function NewAlien(props) {
             
             <div class="flex flex-col mb-4 p-4">
                 <label class="mb-2 font-bold text-lg text-gray-900" for="textarea">챌린지 설명</label>
-                <textarea class="border py-2 px-3 text-grey-800" name="textarea" id="textarea" rows="4"></textarea>
+                <textarea class="border py-2 px-3 text-grey-800" name="textarea" id="textarea" rows="4" value={challengeDescription} onChange={handleChallengeDescription}></textarea>
             </div>
 
             <div class="flex flex-col mb-4 p-4">
@@ -133,13 +133,16 @@ export default function NewAlien(props) {
                 max={100}
                 className="w-full h-3 pr-2 my-4 bg-gray-200 rounded-md cursor-grab"
                 thumbClassName="absolute w-7 h-7 cursor-grab bg-indigo-500 rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 -top-2px"
-/>
+                value={challengeCapacity}
+					      onChange={handleChallengeCapacity}
+                />
+                <span>{challengeCapacity}명</span>
             </div>
 
             
             <div class="flex flex-col mb-4 p-4">
                 <label class="mb-2 font-bold text-lg text-gray-900" for="Select">주 몇회?</label>
-                <select class="border py-2 px-3 text-grey-800">
+                <select class="border py-2 px-3 text-grey-800" value={challengeFrequency} onChange={handleChallengeFrequency}>
                 <option value={SELECT_DEFAULT}>선택</option>
                 <option value="7">매일 참여</option>
                 <option value="6">주 6회 참여</option>
@@ -151,7 +154,7 @@ export default function NewAlien(props) {
                 </select>
             </div>
 
-            
+            <div style={{ color: "#cc3333" }} class="p-2 pl-5 pr-5">{challengeMessage}</div>
             <button class="p-2 pl-5 pr-5 transition-colors duration-700 transform bg-indigo-500 hover:bg-blue-400 text-gray-100 text-lg rounded-lg focus:border-4 border-indigo-300" type="button" onClick={handleSubmit}>챌린지 생성</button>
             
     </div>
