b91871e13d871f481a613d533784337fe38c5cb8
JahunSeo <wkgjs0809@gmail.com>
2021-11-24 17:13:35 +0900

    [johnny] add btn to AlienCtrl

--- client/src/pages/MultiAquarium/AlienCtrl/index.js
+++ client/src/pages/MultiAquarium/AlienCtrl/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link, useMatch } from "react-router-dom";
 
 import {
   // useDispatch,
@@ -7,8 +8,8 @@ import {
 // import * as actions from "../../../Redux/actions";
 
 import styles from "./index.module.css";
-// import classNames from "classnames/bind";
-// const cx = classNames.bind(styles);
+import classNames from "classnames/bind";
+const cx = classNames.bind(styles);
 
 export default function AlienCtrl(props) {
   const { aliens, selectedAlien } = useSelector(({ room }) => ({
@@ -27,15 +28,30 @@ export default function AlienCtrl(props) {
 
 function AlienBox(props) {
   const { alien } = props;
-  //   console.log(alien);
+  const challengeMatch = useMatch("/challenge/:challengeId/room");
+  // const userMatch = useMatch("/user/:userId/room");
+  // const mainMatch = useMatch("/");
+  // console.log(alien);
+
   if (!alien) {
     return <p>생명체를 선택해주세요.</p>;
   } else {
     return (
       <div>
         <p>{`챌린지  : ${alien.challengeName}`}</p>
-        <p>{`생명체  : ${alien.alienName}`}</p>
-        <p>{`인증횟수 : ${alien.accuredAuthCnt}`}</p>
+        <p>{`생명체  : ${alien.alienName} (${alien.accuredAuthCnt}회 인증)`}</p>
+        <div className={styles.btnRow}>
+          <button className={cx("btn", "btn--request")}>인증하기</button>
+          {!!challengeMatch ? (
+            <Link to={`/user/${alien.user_info_id}/room`}>
+              <button className={cx("btn", "btn--room")}>개인 어항</button>
+            </Link>
+          ) : (
+            <Link to={`/challenge/${alien.Challenge_id}/room`}>
+              <button className={cx("btn", "btn--room")}>챌린지 어항</button>
+            </Link>
+          )}
+        </div>
       </div>
     );
   }
