c958285c3d97a16151d8dc7f6ae6f3d5b8bf523a
shinhojung814 <shinhojung814@gmail.com>
2021-11-24 22:35:39 +0900

    [Signal] Authentication Approval Updating

--- client/src/pages/Approval/NoAuthRequest.js
+++ client/src/pages/Approval/NoAuthRequest.js
@@ -1,14 +1,15 @@
 import React from "react";
 import BabyShark from "../../image/babyshark.png";
+// import "NoAuthRequest.css";
 
 const NoAuthRequest = () => {
   const ApprovalButton = () => {
     return (
       <button
         type="button"
-        className="flex min-h-0 min-w-min max-w-sm w-full bg-gradient-to-r from-indigo-600 via-pink-600 to-red-600 hover:from-indigo-500 hover:via-pink-500 hover:to-yellow-500 focus:outline-none text-white text-2xl uppercase font-bold shadow-md rounded-lg mx-auto p-5"
+        className="flex justify-center items-center m-auto min-h-0 min-w-min max-w-sm w-full bg-gradient-to-r from-indigo-600 via-pink-600 to-red-600 hover:from-indigo-500 hover:via-pink-500 hover:to-yellow-500 focus:outline-none text-white text-2xl uppercase font-bold shadow-md rounded-lg mx-auto p-5"
       >
-        <div className="flex sm:flex-cols-12 gap-4">
+        <div className="flex sm:flex-cols-12 gap-2">
           <div className="col-span-1">
             <svg
               className="h-12 w-12"
@@ -31,23 +32,23 @@ const NoAuthRequest = () => {
   };
 
   return (
-    <div className="flex items-center justify-center min-w-min min-h-0 p-12  bg-gray-100">
-      <div className="w-1/4 min-w-min bg-white rounded-lg py-2 shadow-lg hover:shadow-2xl transition duration-500 transform hover:scale-105 cursor-pointer">
-        <img src={BabyShark} alt="authImage" />
+    <div className="flex h-screen min-w-min min-h-0 p-12 bg-center">
+      <div className="flex-auto m-auto w-1/3 min-w-min justify-center rounded-lg py-2 shadow-lg hover:shadow-2xl transition duration-500 transform hover:scale-105 cursor-pointer">
+        <img className="m-auto" src={BabyShark} alt="authImage" />
         <div className="flex flex-col items-center mb-2 space-x-4">
           <div className="mb-2 space-x-4">
-            <div className="mt-6 mb-4 text-2xl font-bold text-black">
+            <div className="justify-center items-center mt-6 mb-4 text-2xl font-bold text-black">
               현재 수락을 기다리는 인증 요청이 없습니다.
             </div>
-            <div className="flex flex-col items-center text-xl font-semibold text-gray-600 mt-2 mb-2">
+            <div className="flex flex-col justify-center items-center text-xl font-semibold text-gray-600 mt-2 mb-2">
               현재 수락을 기다리는 인증 요청이 없습니다.
             </div>
           </div>
         </div>
         <div>
-          <div className="flex min-w-min w-full content-start space-x-4 px-6 py-2">
-            <div className="bg-gray-300 rounded-lg px-2 py-2">
-              <h1 className="px-60 py-2 mb-3 text-xl font-semibold text-black">
+          <div className="flex-col min-w-min w-full justify-center items-center space-x-4 px-6 py-2">
+            <div className="flex-col m-auto bg-gray-300 rounded-lg px-2 py-2">
+              <h1 className="flex justify-center items-center py-2 mb-3 text-xl font-semibold text-black">
                 현재 수락을 기다리는 인증 요청이 없습니다.
               </h1>
             </div>
@@ -57,7 +58,7 @@ const NoAuthRequest = () => {
           <ApprovalButton />
           <button
             type="button"
-            className="flex min-w-min min-h-0 max-w-sm w-full bg-gradient-to-r from-green-500 to-blue-500 hover:from-green-600 hover:to-blue-600 focus:outline-none text-white text-2xl uppercase font-bold shadow-md rounded-lg mx-auto p-5"
+            className="flex justify-center items-center  min-w-min min-h-0 max-w-sm w-full bg-gradient-to-r from-green-500 to-blue-500 hover:from-green-600 hover:to-blue-600 focus:outline-none text-white text-2xl uppercase font-bold shadow-md rounded-lg mx-auto p-5"
           >
             <div className="flex sm:flex-cols-12 gap-2">
               <div className="col-span-1">
@@ -75,7 +76,7 @@ const NoAuthRequest = () => {
                   />
                 </svg>
               </div>
-              <div className="col-span-2">인증 완료</div>
+              <div className="m-auto col-span-2">인증 완료</div>
             </div>
           </button>
         </div>
--- client/src/pages/Approval/index.js
+++ client/src/pages/Approval/index.js
@@ -5,7 +5,6 @@ import api from "../../apis/index.js";
 import NoAuthRequest from "./NoAuthRequest.js";
 
 export default function Approval(props) {
-  // TODO: login 상태일 때만 접근할 수 있음
   const { user } = useSelector(({ user }) => ({ user: user.user }));
 
   const [authRequests, setAuthRequests] = useState([]);
@@ -17,7 +16,6 @@ export default function Approval(props) {
         console.log("res", res);
         console.log("현재 수락을 기다리는 인증 요청이 없습니다.");
       } else {
-        console.log("res", res);
         setAuthRequests(res.data.data);
         return;
       }
@@ -27,7 +25,7 @@ export default function Approval(props) {
 
   console.log("authRequests", authRequests);
 
-  if (authRequests) {
+  if (authRequests.length) {
     return (
       <div className="authRequests">
         {authRequests.map((authRequest) => (
@@ -37,7 +35,7 @@ export default function Approval(props) {
     );
   } else {
     return (
-      <div className="authRequests">
+      <div className="noAuthRequest">
         <NoAuthRequest />
       </div>
     );
@@ -56,22 +54,23 @@ const AuthRequest = ({ authRequest }) => {
   const [approvalStatus, SetApprovalStatus] = useState(false);
 
   const postApproval = async () => {
-    const req = await api.post("/challenge/approve", {
+    const req = await api.post("/challenge/approval", {
       auth_id: authRequest.authentification_id,
       Alien_id: authRequest.alien_id,
       request_date: authRequest.request_id,
     });
     console.log("req", req);
+    if (req.data.result === "success") {
+      alert(`${authRequest.request_user_nickname} 님의 인증을 수락하였습니다.`);
+      SetApprovalStatus(true);
+    }
   };
 
   const handleSubmit = () => {
     postApproval();
-    alert(`${authRequest.request_user_nickname} 님의 인증을 수락하였습니다.`);
-    SetApprovalStatus(true);
   };
 
   const handleNavigate = () => {
-    // navigate(`/user/${authRequest.request_user_id}/room`);
     navigate(`/challenge/${authRequest.challenge_id}/room`);
   };
 
@@ -80,10 +79,10 @@ const AuthRequest = ({ authRequest }) => {
       return (
         <button
           type="button"
-          className="flex min-h-0 min-w-min max-w-sm w-full bg-gradient-to-r from-indigo-500 via-pink-500 to-yellow-500 hover:from-indigo-600 hover:via-pink-600 hover:to-red-600 focus:outline-none text-white text-2xl uppercase font-bold shadow-md rounded-lg mx-auto p-5"
+          className="flex justify-center items-center min-h-0 min-w-min max-w-sm w-full bg-gradient-to-r from-indigo-500 via-pink-500 to-yellow-500 hover:from-indigo-600 hover:via-pink-600 hover:to-red-600 focus:outline-none text-white text-2xl uppercase font-bold shadow-md rounded-lg mx-auto p-5"
           onClick={handleSubmit}
         >
-          <div className="flex sm:flex-cols-12 gap-4">
+          <div className="flex sm:flex-cols-12 gap-2">
             <div className="col-span-1">
               <svg
                 className="h-12 w-12"
@@ -99,7 +98,7 @@ const AuthRequest = ({ authRequest }) => {
                 />
               </svg>
             </div>
-            <div className="col-span-2 pt-1.5">인증 수락</div>
+            <div className="m-auto col-span-2 pt-1.5">인증 수락</div>
           </div>
         </button>
       );
@@ -107,9 +106,9 @@ const AuthRequest = ({ authRequest }) => {
       return (
         <button
           type="button"
-          className="flex min-h-0 min-w-min max-w-sm w-full bg-gradient-to-r from-indigo-600 via-pink-600 to-red-600 hover:from-indigo-500 hover:via-pink-500 hover:to-yellow-500 focus:outline-none text-white text-2xl uppercase font-bold shadow-md rounded-lg mx-auto p-5"
+          className="flex justify-center items-center min-h-0 min-w-min max-w-sm w-full bg-gradient-to-r from-indigo-600 via-pink-600 to-red-600 hover:from-indigo-500 hover:via-pink-500 hover:to-yellow-500 focus:outline-none text-white text-2xl uppercase font-bold shadow-md rounded-lg mx-auto p-5"
         >
-          <div className="flex sm:flex-cols-12 gap-4">
+          <div className="flex sm:flex-cols-12 gap-2">
             <div className="col-span-1">
               <svg
                 className="h-12 w-12"
@@ -125,7 +124,7 @@ const AuthRequest = ({ authRequest }) => {
                 />
               </svg>
             </div>
-            <div className="col-span-2 pt-2">인증 완료</div>
+            <div className="m-auto col-span-2 pt-2">인증 완료</div>
           </div>
         </button>
       );
@@ -134,24 +133,24 @@ const AuthRequest = ({ authRequest }) => {
 
   return (
     <div className="flex items-center justify-center min-w-min min-h-0 p-12  bg-gray-100">
-      <div className="w-1/4 min-w-min bg-white rounded-lg py-2 shadow-lg hover:shadow-2xl transition duration-500 transform hover:scale-105 cursor-pointer">
+      <div className="w-1/3 min-w-min bg-white rounded-lg py-2 shadow-lg hover:shadow-2xl transition duration-500 transform hover:scale-105 cursor-pointer">
         <img src={authRequest.image_url} alt="authImage" />
         <div className="flex flex-col items-center mb-2 space-x-4">
           <div className="mb-2 space-x-4">
-            <div className="mt-6 mb-4 text-2xl font-bold text-black">
+            <div className="justify-center items-center mt-6 mb-4 text-2xl font-bold text-black">
               "{authRequest.request_user_nickname}" 님의 [
               {authRequest.challenge_name}] 인증 요청
             </div>
-            <div className="flex flex-col items-center text-xl font-semibold text-gray-600 mt-2 mb-2">
+            <div className="flex flex-col justify-center items-center text-xl font-semibold text-gray-600 mt-2 mb-2">
               {authYear}년 {authMonth}월 {authDate}일 {authHour}시 {authMinute}
               분에 인증
             </div>
           </div>
         </div>
         <div>
-          <div className="flex min-w-min w-full content-start space-x-4 px-6 py-2">
-            <div className="bg-gray-300 rounded-lg px-2 py-2">
-              <h1 className="px-60 py-2 mb-3 text-xl font-semibold text-black">
+          <div className="flex-col min-w-min w-full justify-center items-center space-x-4 px-6 py-2">
+            <div className="flex-col m-auto bg-gray-300 rounded-lg px-2 py-2">
+              <h1 className="flex justify-center items-center py-2 mb-3 text-xl font-semibold text-black">
                 "{authRequest.comments}"
               </h1>
             </div>
@@ -161,7 +160,7 @@ const AuthRequest = ({ authRequest }) => {
           <ApprovalButton />
           <button
             type="button"
-            className="flex min-w-min min-h-0 max-w-sm w-full bg-gradient-to-r from-green-500 to-blue-500 hover:from-green-600 hover:to-blue-600 focus:outline-none text-white text-2xl uppercase font-bold shadow-md rounded-lg mx-auto p-5"
+            className="flex justify-center items-center min-w-min min-h-0 max-w-sm w-full bg-gradient-to-r from-green-500 to-blue-500 hover:from-green-600 hover:to-blue-600 focus:outline-none text-white text-2xl uppercase font-bold shadow-md rounded-lg mx-auto p-5"
             onClick={handleNavigate}
           >
             <div className="flex sm:flex-cols-12 gap-2">
@@ -180,7 +179,7 @@ const AuthRequest = ({ authRequest }) => {
                   />
                 </svg>
               </div>
-              <div className="col-span-2">챌린지룸 이동</div>
+              <div className="m-auto col-span-2">챌린지룸 이동</div>
             </div>
           </button>
         </div>
