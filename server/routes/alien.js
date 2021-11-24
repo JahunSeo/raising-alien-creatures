0586072d5270fca6fde25b45f42098fc0a5105a9
Ratel <sambujakkk@naver.com>
2021-11-24 20:53:25 +0900

    졸업api변경

--- server/routes/alien.js
+++ server/routes/alien.js
@@ -8,8 +8,9 @@ module.exports = function (pool) {
          // challenge table과 join해서 total_auth_cnt(주 몇회인지) front에서 주 몇회인지 받아오기-> total_auth_cnt insert(매일이면 1, 주 n회이면 n)
          // auth day도 매일하는 challenge이면 어떠한 값이 오는지, 주 n회이면 생명체 생성 시 넘어오는 값 넣기(매일이면 7, n이면 0~6)
         if (req.user) {
+            const image_url = '' + req.body.alien_image
             //body 변수 추가하기
-            const sql1 = `INSERT INTO Alien (user_info_id, Challenge_id, alienName, Alien_image_url, time_per_week, sunday, monday, tuesday, wednesday, thursday, friday, saturday) VALUES (${req.user.id}, ${req.body.challenge_id}, ${req.body.alien_name}, ${req.body.alien_image_url}, ${req.body.total_auth_cnt}, ${req.body.sun}, ${req.body.mon}, ${req.body.tue}, ${req.body.wed}, ${req.body.thu}, ${req.body.fri}, ${req.body.sat});`;
+            const sql1 = `INSERT INTO Alien (user_info_id, Challenge_id, alienName, Alien_image_url, time_per_week, sun, mon, tue, wed, thu, fri, sat) VALUES (${req.user.id}, ${req.body.challenge_id}, ${req.body.alien_name}, ${image_url}, ${req.body.total_auth_cnt}, ${req.body.sun}, ${req.body.mon}, ${req.body.tue}, ${req.body.wed}, ${req.body.thu}, ${req.body.fri}, ${req.body.sat});`;
             // challenge id 받아오기
             const sql2 = `UPDATE Challenge set participantNumber = participantNumber + 1 where id = ${req.body.challenge_id};`;
             // user_info_has_challenge 테이블 row 추가
@@ -55,11 +56,10 @@ module.exports = function (pool) {
     router.get('/graduation', function(req, res){
         // 필요한 데이터: challenge_id, ailen_id
         // 해야할 일 1: alien 테이블 변경
-        const sql1 = 'UPDATE Alien SET status = 1 WHERE id = ?;'
-        // 해야할 일 2: user_info_has_challenge row 삭제
-        const sql2 = 'DELETE FROM user_info_has_Challenge WHERE user_info_id = ? AND Challenge_id = ?;'
+        const sql1 = 'UPDATE Alien SET status = 1, end_date = NOW() WHERE id = ?;'
+        // 해야할 일 2: user_info_has_challenge row 삭제->트리거이용
         // 해야할 일 3: participant - 1
-        const sql3 = 'UPDATE Challenge set participantNumber = participantNumber - 1 where id = ?;'
+        const sql2 = 'UPDATE Challenge set participantNumber = participantNumber - 1 where id = ?;'
         pool.getConnection(function(err, connection) {
             if (err) {
                 console.error(err);
@@ -70,7 +70,7 @@ module.exports = function (pool) {
                 return;
             }
             connection.query(
-                sql1 + sql2 + sql3, [req.body.alien_id, res.user.id, res.body.challenge_id, res.body.challenge_id],
+                sql1 + sql2, [req.body.alien_id, res.body.challenge_id],
                 function (error, results) {
                     if (error) {
                         console.log('at the alien create api', error);
