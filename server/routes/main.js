7b94e15516d850caba9ffeb508ec83a18bf72308
JahunSeo <wkgjs0809@gmail.com>
2021-11-26 23:44:51 +0900

    [johnny] main api edited

--- server/routes/main.js
+++ server/routes/main.js
@@ -6,19 +6,19 @@ module.exports = function (pool) {
     // const sql1 = `(SELECT Alien.id, status, challengeName, challengeContent, Alien.Challenge_id, Challenge.createDate, createUserNickName, maxUserNumber, participantNumber, Alien.createDate, alienName, image_url, accuredAuthCnt, color, end_date FROM Alien JOIN Challenge ON Alien.Challenge_id = Challenge.id) UNION (SELECT Alien_graduated.id, status, challengeName, challengeContent, Alien_graduated.Challenge_id, Challenge.createDate, createUserNickName, maxUserNumber, participantNumber, Alien_graduated.createDate, alienName, image_url, accuredAuthCnt, color, graduated_date FROM Alien_graduated JOIN Challenge ON Alien_graduated.Challenge_id = Challenge.id) ORDER BY accuredAuthCnt DESC LIMIT 50;`;
     pool.getConnection(function (err, connection) {
       if (err) throw err;
-      let columns = `Alien.id, Challenge_id, Alien.createDate as create_date,\
-                  alienName as alien_name, color, accuredAuthCnt as accured_auth_cnt, image_url,\
-                  practice_status, end_date, status,\
-                  time_per_week, sun, mon, tue, wed, thu, fri, sat,\
+      let columns = `alien.id, challenge_id, alien.created_date as alien_created_date,\
+                  alien_name, color, accumulated_count, image_url,\
+                  practice_status, end_date, alien_status,\
+                  alien.times_per_week as times_per_week, sun, mon, tue, wed, thu, fri, sat,\
                   user_info_id, email, nickname as user_nickname,\
-                  challengeName as challenge_name, challengeContent as challenge_content,\
-                  maxUserNumber as max_user_number, participantNumber as participant_number,\
-                  Challenge.createDate as challenge_create_date, cntOfWeek as cnt_of_week`;
-      let sql = `SELECT ${columns} FROM Alien\
-           LEFT JOIN user_info ON Alien.user_info_id=user_info.id\
-           LEFT JOIN Challenge ON Alien.Challenge_id=Challenge.id\
-           WHERE Alien.status=0\
-           ORDER BY accuredAuthCnt DESC LIMIT 50;`;
+                  challenge_name, description,\
+                  maximum_number, participant_number,\
+                  challenge.created_date as challenge_created_date`;
+      let sql = `SELECT ${columns} FROM alien\
+           LEFT JOIN user_info ON alien.user_info_id=user_info.id\
+           LEFT JOIN challenge ON alien.challenge_id=challenge.id\
+           WHERE alien.alien_status=0\
+           ORDER BY accumulated_count DESC LIMIT 50;`;
       connection.query(sql, function (err, results) {
         if (err) throw err;
         res.status(200).json({
