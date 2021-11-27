6a53e717ab2214600501197abcd119635f0784d8
JahunSeo <wkgjs0809@gmail.com>
2021-11-27 22:38:21 +0900

    [johnny] alien ctrl styling

--- client/src/pages/MultiAquarium/AlienCtrl/index.js
+++ client/src/pages/MultiAquarium/AlienCtrl/index.js
@@ -11,6 +11,16 @@ import styles from "./index.module.css";
 import classNames from "classnames/bind";
 const cx = classNames.bind(styles);
 
+const DAY_TEXT = {
+  0: { en: "sun", kr: "일" },
+  1: { en: "mon", kr: "월" },
+  2: { en: "tue", kr: "화" },
+  3: { en: "wed", kr: "수" },
+  4: { en: "thu", kr: "목" },
+  5: { en: "fri", kr: "금" },
+  6: { en: "sat", kr: "토" },
+};
+
 export default function AlienCtrl(props) {
   const { user } = useSelector(({ user }) => ({
     user: user.user,
@@ -24,6 +34,8 @@ export default function AlienCtrl(props) {
   const challengeMatch = useMatch("/challenge/:challengeId/room");
   const userMatch = useMatch("/user/:userId/room");
   // const mainMatch = useMatch("/");
+  const todayValue = new Date().getDay();
+  console.log(1212, todayValue, alien);
 
   // todo 조건 강화!
   if (!alien) {
@@ -35,16 +47,48 @@ export default function AlienCtrl(props) {
   } else {
     return (
       <div className={styles.body}>
-        <p>{`챌린지  : ${alien.challenge_name}`}</p>
-        <p>{`참가자  : ${alien.user_nickname}`}</p>
-        <p>{`생명체  : ${alien.alien_name} (${alien.accumulated_count}회 인증)`}</p>
+        <div className={cx("row")}>
+          <Link to={`/challenge/${alien.challenge_id}/room`}>
+            <h3
+              className={styles.challengeName}
+            >{`${alien.challenge_name}`}</h3>
+          </Link>
+        </div>
+        <div className={cx("row")}>
+          <Link to={`/user/${alien.user_info_id}/room`}>
+            <p className={styles.userName}>
+              {`${alien.user_nickname}`}
+              <span className={styles.authCnt}>
+                {` (${alien.accumulated_count}회 인증)`}
+              </span>
+            </p>
+          </Link>
+        </div>
+        <ul className={styles.daylist}>
+          {[0, 1, 2, 3, 4, 5, 6].map((day) => {
+            let dayType = "default";
+            console.log(111, alien[DAY_TEXT[day].en]);
+
+            if (!!alien[DAY_TEXT[day].en]) dayType = "selected";
+            if (!!alien[DAY_TEXT[day].en] && day === todayValue)
+              dayType = "today";
+            // if (!!alien[day] && (new Date()).getDay() = )
+            return (
+              <li key={day} className={cx("day", `day--${dayType}`)}>
+                {DAY_TEXT[day].kr}
+              </li>
+            );
+          })}
+        </ul>
         <div className={styles.btnRow}>
+          <button className={cx("btn", "btn--request")}>챌린지 어항</button>
+          <button className={cx("btn", "btn--request")}>참가자 어항</button>
           {(!!userMatch || !!challengeMatch) &&
             user.login &&
             user.id === parseInt(alien.user_info_id) && (
-              <button className={cx("btn", "btn--request")}>인증하기</button>
+              <button className={cx("btn", "btn--request")}>인증</button>
             )}
-          {!!challengeMatch ? (
+          {/* {!!challengeMatch ? (
             <Link to={`/user/${alien.user_info_id}/room`}>
               <button className={cx("btn", "btn--room")}>개인 어항</button>
             </Link>
@@ -52,7 +96,7 @@ export default function AlienCtrl(props) {
             <Link to={`/challenge/${alien.challenge_id}/room`}>
               <button className={cx("btn", "btn--room")}>챌린지 어항</button>
             </Link>
-          )}
+          )} */}
         </div>
       </div>
     );
