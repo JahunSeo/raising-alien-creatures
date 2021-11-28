087690bee60c41ffec6dd04f04af5713f60a31d8
JahunSeo <wkgjs0809@gmail.com>
2021-11-29 05:01:01 +0900

    [johnny] delete unused code

--- socket/app.js
+++ socket/app.js
@@ -3,9 +3,6 @@ import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
 
-import Room from "../client/src/shared/room/RoomSocket.mjs";
-import Client from "../client/src/shared/room/Client.mjs";
-
 import * as handler from "./handler.js";
 
 const app = express();
@@ -19,95 +16,13 @@ const options = {
 }; //if needed
 const io = new Server(httpServer, options);
 
-const rooms = {};
-const clients = {};
-
 io.on("connection", (socket) => {
   console.log(`[connection] (clientId) ${socket.id}`);
-  const clientId = socket.id;
-
-  const handleJoin = (info) => {
-    const { roomId, userId } = info;
-    console.log(
-      `[socket server] join ${clientId}, roomId ${roomId}, userId ${userId}`
-    );
-    // 새로운 client 생성
-    if (clients[clientId]) return false; // ERROR
-    const client = new Client({ clientId, userId, roomId });
-    clients[clientId] = client;
-
-    // 해당 room에 client 추가
-    if (!rooms[roomId]) {
-      rooms[roomId] = new Room(roomId);
-      rooms[roomId].start(io);
-    }
-    // client에 room 추가 및 join
-    client.enterRoom(roomId);
-    socket.join(roomId);
-
-    let result = rooms[roomId].addParticipant(client);
-    if (!result) return false; // ERROR
-
-    // broadcasting to all
-    // io.to(roomId).emit("fieldState", rooms[roomId].getFieldState());
-    console.log(
-      `[socket server] join result clientCnt: ${
-        rooms[roomId] && rooms[roomId].clientCnt
-      } / roomCnt: ${Object.keys(rooms).length}`
-    );
-  };
-
-  const handleChangeDestination = (data) => {
-    console.log(`[socket server] changeDestination`, clientId, data);
-    const { roomId, monster } = data;
-    if (!rooms[roomId]) {
-      console.log(`[socket server] changeDestination ERROR!!`);
-      console.log(` ==> there is no room of id ${roomId}`);
-      // TODO: client에 에러 전달?
-      return false;
-    }
-
-    // update monster destination
-    rooms[roomId].updateMonster(clientId, monster);
-
-    // broadcasting to all
-    // io.to(roomId).emit("fieldState", rooms[roomId].getFieldState());
-  };
-
-  const handleDisconnect = () => {
-    // 방에서 참가자 제거
-    console.log(`[socket server] disconnect client ${clientId}`);
-    const client = clients[clientId];
-    if (!client) return false; // ERROR
-
-    const roomId = client.getParticipatingRooms();
-    let remaining_num = rooms[roomId].removeParticipant(client);
-
-    // 유저 제거
-    delete clients[clientId];
-
-    // 방에 참가자가 아무도 없는 경우, 방 제거
-    if (rooms[roomId] && remaining_num <= 0) {
-      // close room
-      rooms[roomId].close();
-      delete rooms[roomId];
-    }
-    // 방에 참가자가 남아 있는 경우, 남은 참가자들에게 전송
-    else {
-      // io.to(roomId).emit("fieldState", rooms[roomId].getFieldState());
-    }
-    console.log(
-      `[socket server] disconnect result clientCnt: ${
-        rooms[roomId] ? rooms[roomId].clientCnt : "none"
-      } / roomCnt: ${Object.keys(rooms).length}`
-    );
-  };
-
   socket.on("join", (data) => handler.join(socket, data));
   socket.on("disconnect", (data) => handler.disconnect(socket, data));
-  // socket.on("on", handleJoin)
-  // socket.on("disconnect", handleDisconnect);
-  socket.on("changeDestination", handleChangeDestination);
+
+  // // // // // // // // // // // // // // // // // // // //
+  // // // // // // // to update // // // // // // // // // //
   socket.on("send_message", (data) => {
     console.log("Server", data);
     socket.to(data.room).emit("receive_message", data);
