8c95a42f170f3e1533a1497143509751951b9e51
JahunSeo <wkgjs0809@gmail.com>
2021-12-15 16:35:28 +0900

    [johnny] add alien warn scheduler to socket server

--- socket/scheduler.js
+++ socket/scheduler.js
@@ -1,5 +1,36 @@
 import schedule from "node-schedule";
 
+export const alienWarnSchedule = (io, rdsClient) => {
+  schedule.scheduleJob(
+    { hour: 14, minute: 30 },
+    // "0,15,30,45 * * * * *",
+    async () => {
+      // 1단계: redis에서 데스노트 가져오기
+      let roomIds = await rdsClient.KEYS("chal-*");
+      let promises = roomIds.map((roomId) => rdsClient.HGETALL(roomId));
+      let results = await Promise.all(promises);
+      // 2단계: 정보 가공해 전송하기
+      results.forEach((challenge) => {
+        // 정보 가공
+        let toasts = [];
+        for (const alienId in challenge) {
+          try {
+            let toast = JSON.parse(challenge[alienId]);
+            toasts.push(toast);
+          } catch (err) {
+            console.error(err);
+          }
+        }
+        // 전송
+        if (toasts.length > 0) {
+          const challengeId = toasts[0].challengeId;
+          io.in(challengeId).emit("thanos_warn", toasts);
+        }
+      });
+    }
+  );
+};
+
 export const alienDeathSchedule = (io, rdsClient) => {
   schedule.scheduleJob(
     { hour: 15, minute: 0 },
@@ -7,10 +38,8 @@ export const alienDeathSchedule = (io, rdsClient) => {
     async () => {
       // 1단계: redis에서 데스노트 가져오기
       let roomIds = await rdsClient.KEYS("chal-*");
-      //   console.log("roomIds", roomIds);
       let promises = roomIds.map((roomId) => rdsClient.HGETALL(roomId));
       let results = await Promise.all(promises);
-      //   console.log("results", results);
       // 2단계: 정보 가공해 전송하기
       results.forEach((challenge) => {
         // 정보 가공
@@ -29,10 +58,7 @@ export const alienDeathSchedule = (io, rdsClient) => {
           io.in(challengeId).emit("thanos_done", toasts);
           //   console.log(challengeId, toasts.length);
         }
-        // 필요시 삭제
-        // rdsClient.DEL(`chal-${challengeId}`);
       });
-
       // 스케쥴러에 추가할 때 참고!
       //   io.emit("thanos_done", { hello: "0000" });
       //   io.in(1).emit("thanos_done", { hello: "1111" });
